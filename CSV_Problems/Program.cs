d6b66edcf6a0d6ea05a7d1128cf539cd392a9b0e
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-28 16:22:34 +0530

    [Prashant] add Program.cs file

---
+++ CSV_Problems/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using CSV_Problems.ReadAndCountRows;
+using CSV_Problems.ReadingCSVFile;
+using CSV_Problems.WritingToCSVFile;
+using CSV_Problems.FilterRecords;
+using CSV_Problems.SearchRecord;
+using CSV_Problems.ModifyCSVFile;
+using CSV_Problems.SortRecords;
+using CSV_Problems.ValidateCSVData;
+using CSV_Problems.DetectDuplicates;
+using CSV_Problems.MergeCSVFiles;
+using CSV_Problems.DataConversion;
+using CSV_Problems.CSVToObjects;
+
+namespace CSV_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ReadFile.UsingStreamReader();
+            // ReadFile.UsingCsvReader();
+
+            // WriteFile.UsingStreamReader();
+            // WriteFile.UsingCsvHelper();
+
+            // CountRows.Execute();
+
+            // ReadFile02.FilterStudent();
+
+            // SearchEmployee.SearchEmployeeByName("Prashant");
+
+            // ModifyFile.UpdateSalary();
+
+            // Sort.SortEmployeesBySalary();
+
+            // Validate.ValidateCSV();
+
+            // Detect.FindDuplicates();
+
+            // Merge.MergeCsvFiles();
+
+            // JSONCSVConverter.Execute();
+
+            // CsvReader.Execute();
+        }
+    }
+}
