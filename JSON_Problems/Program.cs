0209737a1a3c255ca95d37bc095ee3302b604b3f
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-28 23:27:30 +0530

    [Prashant] add Program.cs file

---
+++ JSON_Problems/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using JSON_Problems.IPLAnalyzer;
+
+namespace JSON_Problems
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // IPLAnalyzerMain.Execute();
+            // CreateJsonObject.Execute();
+            // CarToJson.Execute();
+            // MergeJsonObjects.Execute();
+            // FilterJson.Execute();
+            // ListToJsonArray.Execute();
+            // ValidateEmailSchema.Execute();
+            // JsonToXml.Execute();
+            JsonFromDatabase.Execute();
+        }
+    }
+}
