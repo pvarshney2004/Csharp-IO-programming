1eae4eeb76d2f052fc8c8f711ee823fea4a4125e
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-28 16:48:01 +0530

    [Prashant] add Program.cs file

---
+++ Streams_Problems/Program.cs
@@ -0,0 +1,34 @@
+using System;
+using Streams_Problems.BufferedStreamDemo;
+using Streams_Problems.ConvertCase;
+using Streams_Problems.CountWords;
+using Streams_Problems.DataStreamDemo;
+using Streams_Problems.FileStreamDemo;
+using Streams_Problems.MemoryStreamDemo;
+using Streams_Problems.SerializationDeserializationDemo;
+using Streams_Problems.StreamReaderDemo;
+
+namespace Streams_Problems
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // FileReadWrite.Execute();
+
+            // FileCopy.Execute();
+
+            // UserInputToFile.Execute();
+
+            // EmployeeSerialization.Execute();
+
+            // ImageToByteArray.Execute();
+
+            // Count.Execute();
+
+            // UpperToLower.Execute();
+
+            // BinaryStudentData.Execute();
+        }
+    }
+}
